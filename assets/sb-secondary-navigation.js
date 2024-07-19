dec910d465261859288ae3b35546aeba14ce7ea5
egneva <32617539+egneva@users.noreply.github.com>
2024-07-19 18:48:39 +0530

    Delete assets/sb-secondary-navigation.js

--- assets/sb-secondary-navigation.js
+++
@@ -1,8 +0,0 @@
-jQuery(document).ready(function($) {
-    $('.secondary-menu .menu-item-has-children > a').on('click', function(e) {
-        if ($(window).width() <= 768) {
-            e.preventDefault();
-            $(this).parent().toggleClass('submenu-open');
-        }
-    });
-});
