686f6eec51b55d82bd426978c970e3de033d5dee
egneva <32617539+egneva@users.noreply.github.com>
2024-07-19 19:33:38 +0530

    Update sb-secondary-navigation.js

--- assets/js/sb-secondary-navigation.js
+++ assets/js/sb-secondary-navigation.js
@@ -1,8 +1,17 @@
 jQuery(document).ready(function($) {
-    $('.secondary-menu .menu-item-has-children > a').on('click', function(e) {
+    function adjustMenu() {
         if ($(window).width() <= 768) {
-            e.preventDefault();
-            $(this).parent().toggleClass('submenu-open');
+            $('.secondary-menu .menu-item-has-children > a').unbind('click').on('click', function(e) {
+                e.preventDefault();
+                $(this).siblings('.sub-menu').slideToggle();
+                $(this).parent().toggleClass('submenu-open');
+            });
+        } else {
+            $('.secondary-menu .menu-item-has-children > a').unbind('click');
+            $('.secondary-menu .sub-menu').show();
         }
-    });
+    }
+
+    $(window).resize(adjustMenu);
+    adjustMenu();
 });
