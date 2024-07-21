93a6d5eee19158775489073d20bafd6aad4ca37a
egneva <32617539+egneva@users.noreply.github.com>
2024-07-21 12:00:28 +0530

    Update sb-secondary-navigation.js
    Mobile phone menu issue fix.

--- assets/js/sb-secondary-navigation.js
+++ assets/js/sb-secondary-navigation.js
@@ -27,28 +27,49 @@ jQuery(document).ready(function($) {
         
         if (isMobile) {
             $('.secondary-menu .menu-item-has-children > a').off('click').on('click', function(e) {
-
                 const $parentLi = $(this).parent();
                 const $submenu = $parentLi.children('.sub-menu');
                 
-                if ($parentLi.hasClass('submenu-open')) {
-                    $parentLi.removeClass('submenu-open');
-                    $submenu.slideUp();
-                } else {
-                    // Close all other open submenus
-                    $('.secondary-menu .submenu-open').removeClass('submenu-open').children('.sub-menu').slideUp();
+                if (!$parentLi.hasClass('submenu-open')) {
+                    // Close all other open submenus at the same level
+                    $parentLi.siblings('.submenu-open').removeClass('submenu-open').children('.sub-menu').slideUp();
                     
                     // Open this submenu
                     $parentLi.addClass('submenu-open');
                     $submenu.slideDown();
                 }
-                                
+                
                 // Prevent navigation for parent items with children
                 if ($parentLi.hasClass('menu-item-has-children')) {
                     e.preventDefault();
-                    e.stopPropagation();
                 }
+                
+                // Stop propagation to prevent closing when clicking inside
+                e.stopPropagation();
             });
+
+            // Handle clicks on submenu items
+            $('.secondary-menu .sub-menu a').off('click').on('click', function(e) {
+                // If it's a parent item of a nested submenu, toggle it
+                if ($(this).siblings('.sub-menu').length) {
+                    const $parentLi = $(this).parent();
+                    const $submenu = $parentLi.children('.sub-menu');
+                    
+                    if ($parentLi.hasClass('submenu-open')) {
+                        $parentLi.removeClass('submenu-open');
+                        $submenu.slideUp();
+                    } else {
+                        $parentLi.addClass('submenu-open');
+                        $submenu.slideDown();
+                    }
+                    
+                    e.preventDefault();
+                }
+                
+                // Stop propagation to prevent closing parent menus
+                e.stopPropagation();
+            });
+
             navLeft.show();
             navRight.show();
             menu.css('overflow-x', 'auto');
@@ -73,10 +94,5 @@ jQuery(document).ready(function($) {
             $('.secondary-menu .submenu-open').removeClass('submenu-open');
             $('.secondary-menu .sub-menu').slideUp();
         }
-    });
-
-    // Prevent submenu collapse when clicking inside submenu
-    $('.secondary-menu .sub-menu').on('click', function(e) {
-        e.stopPropagation();
     });
 });
